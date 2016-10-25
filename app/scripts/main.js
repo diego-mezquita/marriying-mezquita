db8d9d1b03aa8137d56ef45b0bf36cd3d6bd7cf4
diego-mezquita <diegomezquitafreijoo@gmail.com>
2016-10-26 00:44:11 +0200

    scroll animation to position + highlight current section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,52 @@
 console.log('\'Allo \'Allo!');
 
+$(document).ready(function(){
+
+
+  $('#slides').superslides();
+
+
+});
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active-link');
+        })
+        $(this).addClass('active-link');
+
+        var target = this.hash,
+            menu = target;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.sections-menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.sections-menu nav-menu a').removeClass("active-link");
+            currLink.addClass("active-link");
+        }
+        else{
+            currLink.removeClass("active-link");
+        }
+    });
+}
 
 // $(document).ready(function(){
 //   $('.main').onepage_scroll({
@@ -78,6 +125,20 @@ function ourStoryAnimationApply() {
   }
 
 }
+
+$(window).scroll(function () {
+    $('.main-title').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass('animated fadeOut');
+        } else {
+            $(this).removeClass('animated fadeIn');
+        }
+    });
+});
 
 // $('.js-meet-click').click(function () {
 //     'use strict';
